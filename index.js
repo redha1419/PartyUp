4932d296264a5d686ac079efff53d25461446e42
Reza Shahriari <reza_shahriari@yahoo.ca>
2019-11-02 16:16:35 -0400

    added git ignore

---
+++ index.js
@@ -0,0 +1,26 @@
+const express  = require('express');
+var bodyParser = require("body-parser");
+const app      = express();
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//CORS, to make our server public even when using different location
+ app.use((req, res, next)=> {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+}); 
+
+//grab our routes in users and interface
+const hosting = require('./routes/host'); 
+
+require('dotenv').config(); //grab our environment variables
+
+
+app.use('/', hosting); //set our routes to the "/" location
+
+app.listen(process.env.PORT, function() {
+  console.info("==> 🌎 Peep port %s.", process.env.PORT);
+})
