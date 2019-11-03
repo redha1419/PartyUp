ef74b977b86d20679185526c5916f471c416b1e0
Reza Shahriari <reza_shahriari@yahoo.ca>
2019-11-02 22:14:11 -0400

    added player

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -3,6 +3,8 @@ import logo from '../logo.svg';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import '../App.css';
+import axios from 'axios'
+import SpotifyWebPlayer from 'react-spotify-web-playback';
 
 const styles = theme => ({
   button: {
@@ -13,10 +15,59 @@ const styles = theme => ({
   },
 });
 
+export const clientId = "ca01bcb9c7bb4fcba110e037d4206258"
+export const authEndpoint = 'https://accounts.spotify.com/authorize?';
+export const redirectUri = "http://localhost:3000";
+export const scopes = [
+  "streaming",
+  "user-read-email",
+  "user-read-private",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "app-remote-control",
+  "user-modify-playback-state",
+"user-read-currently-playing",
+"user-read-playback-state"
+];
+
+// Get 
+
+
+//https://accounts.spotify.com/authorizeclient_id=508e71ca079c47459d2e61a4fb3ca5fe&redirect_uri=http://localhost:3000/verify&scope=user-read-currently-playing%20user-read-playback-state&response_type=token&show_dialog=true
+
+
+//https://accounts.spotify.com/authorize/client_id=508e71ca079c47459d2e61a4fb3ca5fe&redirect_uri=http://localhost:3000&scope=user-read-currently-playing%20user-read-playback-state&response_type=token&show_dialog=true
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+window.location.hash = "";
 
 class HomePage extends React.Component {
   constructor(props){
     super(props)
+    this.state ={
+      token: ""
+    }
+  }
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+    console.log(_token)
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
   }
   render() {
     const{ classes } = this.props
@@ -28,15 +79,23 @@ class HomePage extends React.Component {
           Welcome
         </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
 
-        </h1>
-        <Button variant="contained" color="primary" className={classes.button} onClick={()=>{this.props.history.push('/user')}}>
-        Joining a Party?
+        {!this.state.token && (
+        <Button variant="contained" color="primary" className={classes.button}
+         href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+        >
+         Hosting a Party?
+        </Button>
+        )}
+      <Button variant="contained" color="secondary" className={classes.button} onClick={()=>{this.props.history.push('/user')}}>
+      Joining a Party?
       </Button>
-      <Button variant="contained" color="secondary" className={classes.button} onClick={()=>{this.props.history.push('/verify')}}>
-        Hosting a Party?
-      </Button>
+          {this.state.token && (
+                    <SpotifyWebPlayer
+                    token={this.state.token}
+                    uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']}
+                />
+          )}
         
         
       </header>
