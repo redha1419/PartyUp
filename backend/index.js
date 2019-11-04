d343bc64b5e2d6f2f5a54fffe2b1b96dcf1600fe
Reza Shahriari <reza_shahriari@yahoo.ca>
2019-11-04 16:41:58 -0500

    ads

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,8 @@ app.use(bodyParser.json());
 
 //CORS, to make our server public even when using different location
  app.use((req, res, next)=> {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
